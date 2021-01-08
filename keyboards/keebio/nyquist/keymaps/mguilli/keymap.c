fee2cdb6763e2e94f0fafac8b8a238b32db4227b
mguilli <matthew.guilliams@gmail.com>
2021-01-07 21:02:35 -0600

    Reversed alt and os keys on bottom row

--- keyboards/keebio/nyquist/keymaps/mguilli/keymap.c
+++ keyboards/keebio/nyquist/keymaps/mguilli/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SftEnt|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |CtlSft| GUI  | Alt  |Lower |SpcNav|SpcNav|Raise | Left | Down |  Up  |Right |
+ * | Ctrl |CtlSft| Alt  | OS  |Lower |SpcNav|SpcNav|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
-  KC_LCTL, CTL_SFT, KC_LGUI, KC_LALT, LOWER, SPC_NAV, SPC_NAV, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_LCTL, CTL_SFT, KC_LALT, KC_LGUI, LOWER, SPC_NAV, SPC_NAV, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
