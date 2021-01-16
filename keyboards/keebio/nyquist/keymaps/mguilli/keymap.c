4f099e1926db21a967ecb7e841ae82409424d5b0
mguilli <matthew.guilliams@gmail.com>
2021-01-16 08:59:47 -0600

    Reorganized symbol layer

--- keyboards/keebio/nyquist/keymaps/mguilli/keymap.c
+++ keyboards/keebio/nyquist/keymaps/mguilli/keymap.c
@@ -79,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   |  |   :  |   \  |   [  |   {  |   }  |   ]  |   /  |   -  |   +  |  |   |
+ * |      |   =  |   _  |   [  |   (  |   {  |   }  |   )  |   ]  |   -  |   +  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |   _  |   =  |      |
+ * |      |      |   <  |   /  |   |  |      |      |   |  |   \  |   >  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-  _______, KC_PIPE, KC_COLN, KC_BSLS, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, KC_SLSH, KC_MINS, KC_PLUS, KC_PIPE, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_EQL,  _______, \
+  _______, KC_EQL,  KC_UNDS, KC_LBRC, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_RBRC, KC_MINS, KC_PLUS, KC_DQUO, \
+  _______, XXXXXXX, KC_LT,   KC_SLSH, KC_PIPE, XXXXXXX, XXXXXXX, KC_PIPE, KC_BSLS, KC_GT,   XXXXXXX, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
