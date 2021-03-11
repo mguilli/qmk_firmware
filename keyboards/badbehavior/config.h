eafeae3395f833db01ebd899548ca65e08fd9ba5
mguilli <matthew.guilliams@gmail.com>
2021-03-11 17:36:01 -0600

    Changed pins on MCU assignments:

--- keyboards/badbehavior/config.h
+++ keyboards/badbehavior/config.h
@@ -10,14 +10,14 @@
 #define MATRIX_COLS 14
 
 #define MATRIX_ROW_PINS {D3, D2, D1, B0, F4}
-#define MATRIX_COL_PINS {D0, D4, C6, D7, E6, B4, B5, F5, F6, F7, B1, B3, B2, B6}
+#define MATRIX_COL_PINS {F6, F5, F7, B1, B3, B2, B6, B5, B4, E6, D7, C6, D4, D0}
 #define UNUSED_PINS {C7}
 
 #define DIODE_DIRECTION COL2ROW
 
 #define DEBOUNCE 5
 
-#define ENCODERS_PAD_A { B7, D5 }
-#define ENCODERS_PAD_B { F1, F0 }
+#define ENCODERS_PAD_A { F0, D5 }
+#define ENCODERS_PAD_B { F1, B7 }
 
 #define ENCODER_RESOLUTION 4
