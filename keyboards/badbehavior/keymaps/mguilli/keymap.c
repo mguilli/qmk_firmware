e175f3cbe0df371476425b4d6810c661e6bd8791
mguilli <matthew.guilliams@gmail.com>
2021-03-23 08:15:16 -0500

    Added back control key badbehavior

--- keyboards/badbehavior/keymaps/mguilli/keymap.c
+++ keyboards/badbehavior/keymaps/mguilli/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_LPRN,     KC_RPRN, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,         KC_BSLS, \
       ESC_CTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,     KC_MUTE, KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_COLON), KC_QUOT, \
       KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    BRC_FUN,     KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      SFT_ENT, \
-      XXXXXXX, ADJUST, KC_LGUI, KC_LALT, XXXXXXX, KC_BSPC, TD(DEL_NUM), KC_ENT,  SPC_NAV, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,        KC_RGHT  \
+      KC_LCTL, ADJUST, KC_LGUI, KC_LALT, XXXXXXX, KC_BSPC, TD(DEL_NUM), KC_ENT,  SPC_NAV, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,        KC_RGHT  \
       ),
 
 /* Nav
